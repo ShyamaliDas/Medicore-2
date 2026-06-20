ecebe727e0f71bd95abf1b88608e39a2334be8d6
Rahad0Islam <vairahad99@gmail.com>
2026-06-20 22:27:23 +0600

    Update pom.xml for Spring Boot version and add dependencies; implement JwtInterceptor and WebConfig for JWT authentication

---
+++ Back-end/services/userservice/src/main/java/service/userservice/userservice/config/WebConfig.java
@@ -0,0 +1,19 @@
+package service.userservice.userservice.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Autowired
+    private JwtInterceptor jwtInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor)
+                .addPathPatterns("/api/v1/user/**", "/api/v1/patient/**", "/api/v1/doctor/**", 
+                                 "/api/v1/admin/**", "/api/v1/pharmacist/**");
+    }
+}
