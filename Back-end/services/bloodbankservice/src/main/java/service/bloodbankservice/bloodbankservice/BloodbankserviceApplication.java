623f2d802cd9f0973b1030cee7c310038b6e5fbe
AD-053 <autanu2025@gmail.com>
2026-06-22 22:25:12 +0600

    initialize blood bank service

---
+++ Back-end/services/bloodbankservice/src/main/java/service/bloodbankservice/bloodbankservice/BloodbankserviceApplication.java
@@ -0,0 +1,13 @@
+package service.bloodbankservice.bloodbankservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodbankserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodbankserviceApplication.class, args);
+	}
+
+}
