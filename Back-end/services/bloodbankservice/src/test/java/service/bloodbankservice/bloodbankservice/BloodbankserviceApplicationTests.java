18d7d58adae9a2144208742e5977cd89dd9b937b
AD-053 <autanu2025@gmail.com>
2026-06-23 17:10:23 +0600

    testing

--- Back-end/services/bloodbankservice/src/test/java/service/bloodbankservice/bloodbankservice/BloodbankserviceApplicationTests.java
+++ Back-end/services/bloodbankservice/src/test/java/service/bloodbankservice/bloodbankservice/BloodbankserviceApplicationTests.java
@@ -6,9 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BloodbankserviceApplicationTests {
 
-	@Test //demo test
+	@Test // new demo comment
 	void contextLoads() {
 
 	}
+	// add demo comment
 
 }
